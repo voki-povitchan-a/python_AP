7ac42235cffdbc1c9f156b89d087e68c3b3fac15
Nastya <s4astliva.ya1@gmail.com>
2022-01-17 20:44:37 +0200

    ESCAPE output

---
+++ escape_sequences.py
@@ -0,0 +1,10 @@
+print('''
+    Escape sequences
+    \\a      Bell (alert)
+    \\b      Backspace
+    \\n      New line
+    \\t      Horizontal tab
+    \\\\      Backslash \\
+    \\'      Single quotation mark "
+    \\"      Double quotation mark '
+''')
